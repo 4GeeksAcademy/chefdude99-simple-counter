3f86560e896ddbf2d754d68fa8f7db41d4865fb8
chefdude99 <chefdude.1999@gmail.com>
2025-02-28 23:08:24 +0000

    css adelantado

---
+++ src/js/components/Contador.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Contador = () => {
+    return (
+        <div className="ContadorGeneral">
+            <div className="OtrosIconos">
+                <i className="fa-solid fa-clock"></i>
+            </div>
+            <div className="OtrosIconos">
+                <p>y</p>
+            </div>
+            <div className="OtrosIconos">
+                <p>a</p>
+            </div>
+            <div className="OtrosIconos">
+                <p>a</p>
+            </div>
+            <div className="OtrosIconos">
+                <p>a</p>
+            </div>
+            <div className="OtrosIconos">
+                <p>p</p>
+            </div>
+            <div className="OtrosIconos">
+                <p>l</p>
+            </div>
+            <div className="OtrosIconos">
+                <p>a</p>
+            </div>
+        </div>
+    )
+
+}
+
